c1c50cd49cd3d5d81dc1a120b3c13db933a0cfbb
Dereje Seifu <Derejeseifu3030@gmail.com>
2024-04-23 07:00:02 +0300

    custom modal created

--- app/feature/profile/Profile.tsx
+++ app/feature/profile/Profile.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { View, Image, StyleSheet, Text, Modal, Pressable, Alert } from 'react-native'
+import { View, Image, StyleSheet, Text, Modal, Pressable, Alert, TextInput } from 'react-native'
 import SvgUri from 'react-native-svg-uri'
 import { useAuth } from '../../store/hooks/useAuth'
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Divider } from '@rneui/base';
+import { Button, Divider } from '@rneui/base';
+import CustomModal from './components/CustomModal';
 
 type Props = {
     navigation: StackNavigationProp<any, 'Home'>; // Define the navigation prop type
@@ -11,7 +12,15 @@ type Props = {
 
 const Profile: React.FC<Props> = ({ navigation }) => {
     const { session } = useAuth()
-    const [modalVisible, setModalVisible] = useState(true);
+    const [editNameModalVisibile, setEditNameModalVisible] = useState(false);
+    const [editUserNameModalVisibile, setEditUserNameModalVisible] = useState(false);
+    const [editEmailModalVisibile, setEdiEmailModalVisible] = useState(false);
+    const [editAddressModalVisibile, setEditAddressModalVisible] = useState(false);
+    const [firstName, setFirstName] = useState<string>(session?.userInfo.firstName ?? '')
+    const [lastName, setLastName] = useState<string>(session?.userInfo.lastName ?? '')
+    const [userName, setUserName] = useState<string>(session?.userInfo.userName ?? '')
+    const [email, setEmail] = useState<string>(session?.userInfo.email ?? '')
+    const [address, setAddress] = useState<string>(session?.userInfo.address ?? '')
     return (
         <View style={styles.page}>
             <View style={styles.item}>
@@ -32,7 +41,7 @@ const Profile: React.FC<Props> = ({ navigation }) => {
                     />
                     <Text>Name</Text>
                 </View>
-                <Text style={styles.textLink}>{`${session?.userInfo.firstName} ${session?.userInfo.lastName}`}</Text>
+                <Text style={styles.textLink} onPress={() => setEditNameModalVisible(true)}>{`${session?.userInfo.firstName} ${session?.userInfo.lastName}`}</Text>
             </View>
             <Divider/>
             <View style={styles.item}>
@@ -44,7 +53,7 @@ const Profile: React.FC<Props> = ({ navigation }) => {
                     />
                     <Text>Username</Text>
                 </View>
-                <Text style={styles.textLink}>{session?.userInfo.userName}</Text>
+                <Text style={styles.textLink} onPress={() => setEditUserNameModalVisible(true)}>{session?.userInfo.userName}</Text>
             </View>
             <Divider/>
             <View style={styles.item}>
@@ -56,7 +65,7 @@ const Profile: React.FC<Props> = ({ navigation }) => {
                     />
                     <Text>Email</Text>
                 </View>
-                <Text style={styles.textLink}>{session?.userInfo.email}</Text>
+                <Text style={styles.textLink} onPress={() => setEdiEmailModalVisible(true)}>{session?.userInfo.email}</Text>
             </View>
             <Divider/>
             <View style={styles.item}>
@@ -68,28 +77,115 @@ const Profile: React.FC<Props> = ({ navigation }) => {
                     />
                     <Text>Address</Text>
                 </View>
-                <Text style={styles.textLink}>{session?.userInfo.address}</Text>
+                <Text style={styles.textLink} onPress={() => setEditAddressModalVisible(true)}>{session?.userInfo.address}</Text>
             </View>
             <Divider/>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                Alert.alert('Modal has been closed.');
-                setModalVisible(!modalVisible);
-                }}>
-                <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <Text style={styles.modalText}>Hello World!</Text>
-                    <Pressable
-                    style={[styles.button, styles.buttonClose]}
-                    onPress={() => setModalVisible(!modalVisible)}>
-                    <Text style={styles.textStyle}>Hide Modal</Text>
-                    </Pressable>
-                </View>
-                </View>
-            </Modal>
+            {
+                editNameModalVisibile && 
+                <CustomModal
+                    open={editNameModalVisibile}
+                    onClose={setEditNameModalVisible}
+                    title='Edit Name'
+                >
+                    <View style={styles.modalView}>
+                        <View style={styles.inputItem}>
+                            <Text>First Name</Text>
+                            <TextInput 
+                                placeholder="First Name" 
+                                value={firstName}
+                                onChangeText={setFirstName}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <View style={styles.inputItem}>
+                            <Text>Last Name</Text>
+                            <TextInput 
+                                placeholder="Last Name" 
+                                value={lastName}
+                                onChangeText={setLastName}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <Button
+                            title='SUBMIT'
+                            style={styles.button}
+                        />
+                    </View>
+                </CustomModal>
+            }
+            {
+                editUserNameModalVisibile && 
+                <CustomModal
+                    open={editUserNameModalVisibile}
+                    onClose={setEditUserNameModalVisible}
+                    title='Edit User Name'
+                >
+                    <View style={styles.modalView}>
+                        <View style={styles.inputItem}>
+                            <Text>User Name</Text>
+                            <TextInput 
+                                placeholder="User Name" 
+                                value={userName}
+                                onChangeText={setUserName}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <Button
+                            title='SUBMIT'
+                            style={styles.button}
+                        />
+                    </View>
+                </CustomModal>
+            }
+            {
+                editEmailModalVisibile && 
+                <CustomModal
+                    open={editEmailModalVisibile}
+                    onClose={setEdiEmailModalVisible}
+                    title='Edit Email'
+                >
+                    <View style={styles.modalView}>
+                        <View style={styles.inputItem}>
+                            <Text>Email</Text>
+                            <TextInput 
+                                placeholder="Email" 
+                                value={email}
+                                onChangeText={setEmail}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <Button
+                            title='SUBMIT'
+                            style={styles.button}
+                        />
+                    </View>
+                </CustomModal>
+            }
+            {
+                editAddressModalVisibile && 
+                <CustomModal
+                    open={editAddressModalVisibile}
+                    onClose={setEditAddressModalVisible}
+                    title='Edit Address'
+                >
+                    <View style={styles.modalView}>
+                        <View style={styles.inputItem}>
+                            <Text>Address</Text>
+                            <TextInput 
+                                placeholder="Address" 
+                                value={address}
+                                onChangeText={setAddress}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <Button
+                            title='SUBMIT'
+                            style={styles.button}
+                        />
+                    </View>
+                </CustomModal>
+            }
+
         </View>
     )
 }
@@ -101,11 +197,21 @@ const styles = StyleSheet.create({
         backgroundColor: "#FFFFFF",
         height: "100%"
     },
+    inputItem: {
+        flexDirection: "column",
+        justifyContent: "space-between",
+        width: "100%",
+        gap: 8,
+        
+    },
     item: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         marginHorizontal: 10
+    },
+    label: {
+        color: "#AFADAD"
     },
     textWithSVG: {
         flexDirection: "row",
@@ -120,47 +226,29 @@ const styles = StyleSheet.create({
     textLink: {
         color: "#9F520B"
     },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
-      },
-      modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-      },
-      button: {
+    modalView: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 20
+    },
+    // textInput: {
+    //     width: "100%"
+    // },
+    textInput:{
+        width: "100%",
+        borderWidth: 1,
+        padding: 10,
+        borderRadius:5,
+        borderColor: "#AFADAD",
+        backgroundColor:'white',
+        color:'#000000',
+    },
+    button: {
         borderRadius: 20,
         padding: 10,
         elevation: 2,
-      },
-      buttonOpen: {
-        backgroundColor: '#F194FF',
-      },
-      buttonClose: {
-        backgroundColor: '#2196F3',
-      },
-      textStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-      },
-      modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
-      },
+        width: "100%"
+    },
 })
 
 export default Profile
